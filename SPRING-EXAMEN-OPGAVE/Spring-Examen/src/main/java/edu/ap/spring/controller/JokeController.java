04ca5e8c3ad2abd40e6e7c993dff02b08d84a9d9
SilkeHenderickx <silke.henderickx@student.ap.be>
2018-09-06 14:16:26 +0200

    Get json from url

--- SPRING-EXAMEN-OPGAVE/Spring-Examen/src/main/java/edu/ap/spring/controller/JokeController.java
+++ SPRING-EXAMEN-OPGAVE/Spring-Examen/src/main/java/edu/ap/spring/controller/JokeController.java
@@ -4,6 +4,11 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 @Controller
 @Scope("session")
 public class JokeController {
@@ -17,12 +22,38 @@ public class JokeController {
    }
 		   
    @RequestMapping("/joke_post")
+   @ResponseBody
    public String joke_post() {
-	   return "";
+
+       return null;
+
    }
    
    @RequestMapping("/")
    public String root() {
 	   return "redirect:/joke";
    }
+
+    public static String jsonGetRequest(String firstName, String lastName) throws Exception {
+        String url = "http://api.icndb.com/jokes/random?firstName=" + firstName + "&lastName=" + lastName;
+        URL obj = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+        // optional default is GET
+        con.setRequestMethod("GET");
+        //add request header
+        con.setRequestProperty("User-Agent", "Mozilla/5.0");
+        int responseCode = con.getResponseCode();
+        System.out.println("\nSending 'GET' request to URL : " + url);
+        System.out.println("Response Code : " + responseCode);
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+        //print in String
+        return response.toString();
+    }
 }
